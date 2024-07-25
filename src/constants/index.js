7523f687669ada3d19c41a9c02dc642b61dc9fed
gowtham <gowthamv0210@gmail.com>
2024-07-25 19:26:10 +0530

    minor changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const ORD_API = "http://api.openweathermap.org/geo/1.0/direct?";
+export const ORD_API = "https://api.openweathermap.org/geo/1.0/direct?";
 export const CW_API = "https://api.openweathermap.org/data/2.5/weather?";
-export const TIMEZONE_API = "http://api.timezonedb.com/v2.1/get-time-zone?";
+export const TIMEZONE_API = "https://api.timezonedb.com/v2.1/get-time-zone?";
